4b5d4021190a8e98f32d691e9a6e5c7ce247872c
sergio <herrsergio@gmail.com>
2020-11-23 13:50:50 -0600

    Added new authors/people

--- wikiquotes_serverless.py
+++ wikiquotes_serverless.py
@@ -24,16 +24,28 @@ def wikiquotesRandom(event, context):
                "Albert Einstein", "Gautama Buddha",
                "Isaac Newton", "Johann Wolfgang von Goethe",
                "Bill Gates", "Linus Torvalds",
-               "Octavio Paz"]
+               "Octavio Paz", "Plato", "Fyodor Dostoyevsky",
+               "Hermann Hesse", "Franz Kafka", "Aristotle",
+               "René Descartes", "Jacques Lacan", "Benito Juárez",
+               "José Vasconcelos", "Anton LaVey", "Martin Luther King",
+               "Desmond Tutu", "Mahatma Gandhi", "Napoleon I of France",
+               "Emiliano Zapata", "Abraham Lincoln", "Vladimir Lenin",
+               "Karl Marx", "René Descartes", "Anthony Burgess",
+               "Anthony Bourdain", "Sun Tzu", "Alan Turing", "Ada Lovelace",
+               "Dennis Ritchie", "Ken Thompson", "Simón Bolívar",
+               "Diego Armando Maradona", "Rammstein", "Bono",
+               "Bruce Lee", "Krishna", "Immanuel Kant", "Galileo Galilei"]
 
     author = random.choice(authors)
 
-    if (author == "Jorge Luis Borges") or (author == "Octavio Paz"):
+    if (author == "Jorge Luis Borges") or (author == "Octavio Paz") or (author == "Benito Juárez")
+    or (author == "José Vasconcelos") or (author == "Emiliano Zapata") or (author == "Simón Bolívar")
+    or (author == "Diego Armando Maradona"):
         lang = "spanish"
 
     a = wikiquotes.random_quote(author, lang)
 
-    while len(a)>260:
+    while len(a)>280:
        a = wikiquotes.random_quote(author, lang)    
 
     message = a+"\n\n"+author
